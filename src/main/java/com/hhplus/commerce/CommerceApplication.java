2b58233c8d5a1a8ad8a7df9a4cfc24a81c5793f7
sunmon <blackmilk274@gmail.com>
2024-10-07 22:20:25 +0900

    init project
    - 프로젝트 생성
    - gitignore 설정
    - dependency 설정

---
+++ src/main/java/com/hhplus/commerce/CommerceApplication.java
@@ -0,0 +1,18 @@
+package com.hhplus.commerce;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+    private static final Logger logger = LoggerFactory.getLogger(CommerceApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceApplication.class, args);
+        logger.info("Application started");
+
+    }
+
+}
