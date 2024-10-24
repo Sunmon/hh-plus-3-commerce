8b18e2d6cc124ff89b910f69fc01ac3e65f93a9b
sunmon <blackmilk274@gmail.com>
2024-10-25 03:04:07 +0900

    feat: 어플리케이션 시작 로그 추가
    Issue #29

--- src/main/java/com/hhplus/commerce/CommerceApplication.java
+++ src/main/java/com/hhplus/commerce/CommerceApplication.java
@@ -1,21 +1,22 @@
 package com.hhplus.commerce;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableJpaRepositories
 public class CommerceApplication {
-    private static final Logger logger = LoggerFactory.getLogger(CommerceApplication.class);
 
     public static void main(String[] args) {
+        log.info("Application Started {}", LocalDateTime.now());
         SpringApplication.run(CommerceApplication.class, args);
-        logger.info("Application started");
     }
 
 }
